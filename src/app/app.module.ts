14a184f271bc727435f32db71bce6e67b97b76a9
saretoduncan <saretoduncan@gmail.com>
2021-07-17 13:15:59 +0300

    import form modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuotesGenComponent } from './quotes-gen/quotes-gen.component';
-import { FormComponent } from './form/form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuotesGenComponent } from "./quotes-gen/quotes-gen.component";
+import { FormComponent } from "./form/form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuotesGenComponent,
-    FormComponent
-  ],
+  declarations: [AppComponent, QuotesGenComponent, FormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
