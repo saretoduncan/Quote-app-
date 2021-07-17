443071ebcfa95091cb580b6ab376577325bcbd81
saretoduncan <saretoduncan@gmail.com>
2021-07-17 11:43:38 +0300

    import model class and create a new quote generate method

--- src/app/quotes-gen/quotes-gen.component.ts
+++ src/app/quotes-gen/quotes-gen.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ModelClass } from "../custom-class/model-class";
 
 @Component({
-  selector: 'app-quotes-gen',
-  templateUrl: './quotes-gen.component.html',
-  styleUrls: ['./quotes-gen.component.css']
+  selector: "app-quotes-gen",
+  templateUrl: "./quotes-gen.component.html",
+  styleUrls: ["./quotes-gen.component.css"],
 })
 export class QuotesGenComponent implements OnInit {
+  quoteGenerator: ModelClass[] = [new ModelClass("duncan", "never give up")];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
